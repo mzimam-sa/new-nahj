7c277d20bdd939353e7915e6131d5167852a8b06
manar95ahsa <manar@asmartech.ae>
2026-04-27 12:19:57 +0300

    change script

--- public/run.php
+++ public/run.php
@@ -1,6 +1,6 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require __DIR__ . '/bootstrap/app.php';
+require __DIR__ . '/../vendor/autoload.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
 $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
 $student = App\User::find(1077);
